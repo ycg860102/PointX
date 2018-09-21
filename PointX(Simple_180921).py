b08efbfcb10e105409a6a2f3795f3eed6b9f1f00
ycg860102 <ycg860102@163.com>
2018-09-21 17:11:18 +0800

    修改为通过天软调取行情数据
    并可以将数据写入数据库中

--- PointX(Simple_180921).py
+++ PointX(Simple_180921).py
@@ -14,6 +14,8 @@ Created on Fri Sep 20 09:03:07 2018
         ②如果有阻力线，没有支撑线，则处于空头状态；
         ③既有阻力又有支撑，则处于震荡整理阶段                        
 
+    20180921 修改获取股价信息为通过天软获取
+
 """
 
 import pandas as pd
@@ -28,6 +30,7 @@ import os
 from multiprocessing import Pool
 import WindPy as wp 
 import PointXTool as PointX
+from sqlalchemy import create_engine 
 
 import sys
 sys.path.append("D:\Program Files\Tinysoft\Analyse.NET") 
@@ -46,8 +49,12 @@ def getGraduation(price):
         return 0.5
     elif price <=100:
         return 1.0
-    else:
+    elif price<=200:
         return 2.5
+    elif price<=1000:
+        return 5
+    else:
+        return 10
 
 def DealDataHL( titleName, sourceDatas,MinSize = 0.05 ):
     #函数说明：如果上涨阶段，反转的条件有两种： 1、最高价没有创新高，则当天最高价比前一列最高价低三个维度 
@@ -197,7 +204,7 @@ def getStockListClosePrice(Stocklist,begDay,endDay,args):
 def getStockListPriceTSL(Stocklist,begDay,endDay,args) :
     prices= TSLPy2.RemoteCallFunc('getStocksPrice',[Stocklist,begDay,endDay,args],{})   
     prices = prices[1]
-    return prices 
+    return pd.DataFrame(prices).T 
    
 def findZCZLLine(data1,N,isFindMax=0,isFindMin=0,startXtoEnd=0):
     #增加趋势线，1、找到4阶枢纽拐点，2，枢纽拐点往右新增一格，增加或减少一个graduation，直到最早一天数据
@@ -255,38 +262,41 @@ def findZCZLLine(data1,N,isFindMax=0,isFindMin=0,startXtoEnd=0):
 if __name__ == "__main__":
     
     print "Begining ------>" 
+    engine = create_engine('mysql://root:root@127.0.0.1/dwlh?charset=utf8')#用sqlalchemy创建引擎  
     now = datetime.datetime.now()  
 
-    toDay = now.strftime('%Y-%m-%d')
+    toDay = now.strftime('%Y%m%d')
     #toDay = "2017-12-12"
     
     #下面的90，代表九十天前 365天为过去一年暑假
     begDay = now + datetime.timedelta(days=-365)
+    begDay = begDay.strftime('%Y%m%d')
     #begDay='2017-05-10'
     #closeData = getCloseData("close",toDay)
     #填写需要计算的股票代码    
     
     #stockList =  ['600132.SH','000848.SZ'] 
     #bk = u'沪深300'
-    bk = u'上证50'
+    bk = u'中证800'
     stockList = TSLPy2.RemoteCallFunc('getbk',[bk],{})   
     stockList =  stockList[1]
-    stockList = [ stock[2:8]+'.'+stock[0:2] for stock in stockList ] 
+
+    #stockList = [ stock[2:8]+'.'+stock[0:2] for stock in stockList ] 
     #,'601318.SH','002883.SZ','600066.SH','603578.SH']     
     #stockList =  ['600578.SH','600172.SH','000848.SZ','600993.SH','600446.SH','600519.SH']
-    stockList =  ['600028.SH',]
+    stockList =  ['SH000001',]
     #stockList =  ['601328.SH','000001.SZ','600015.SH','000932.SZ','600782.SH','600282.SH','600022.SH','000959.SZ','600569.SH','600507.SH','000778.SZ','600126.SH','300003.SZ','002223.SZ','603658.SH','300463.SZ','300529.SZ','600529.SH','002022.SZ','601233.SH','603225.SH','600810.SH','002064.SZ','002206.SZ','300122.SZ','000661.SZ','002252.SZ','600161.SH','300294.SZ','300485.SZ','300009.SZ','600118.SH','300456.SZ','600138.SH','603869.SH','300178.SZ','000547.SZ','002482.SZ','300506.SZ','002713.SZ','600511.SH','000028.SZ','002589.SZ','603368.SH','002092.SZ','600409.SH','000683.SZ','603077.SH','000822.SZ','002648.SZ','600618.SH','600277.SH','600328.SH','601158.SH','000544.SZ','600820.SH','600326.SH','600512.SH','600502.SH','002460.SZ','000960.SZ','600392.SH','002716.SZ','600459.SH','600585.SH','600801.SH','000401.SZ','002233.SZ','600720.SH','000672.SZ','603986.SH','600667.SH','603501.SH','300373.SZ','002371.SZ','002049.SZ','600460.SH','601766.SH','000008.SZ','600970.SH','002051.SZ','000065.SZ','600496.SH','000636.SZ','603328.SH','002815.SZ','300476.SZ','600563.SH','002138.SZ','002463.SZ','600580.SH','000922.SZ','603988.SH','601966.SH','601058.SH','002068.SZ','300144.SZ','002310.SZ','300197.SZ','002717.SZ','300237.SZ','300349.SZ','300410.SZ','300114.SZ','002658.SZ','300572.SZ','300066.SZ'] 
     #N=2
     Nlist = [1,]
-    closeData = getStockListClosePrice(stockList,begDay,toDay,'close') 
-    highData = getStockListClosePrice(stockList,begDay,toDay,'high') 
-    lowData = getStockListClosePrice(stockList,begDay,toDay,'low') 
-    openData = getStockListClosePrice(stockList,begDay,toDay,'open') 
+    closeData = getStockListPriceTSL(stockList,int(begDay),int(toDay),'close') 
+    highData = getStockListPriceTSL(stockList,int(begDay),int(toDay),'high') 
+    lowData = getStockListPriceTSL(stockList,int(begDay),int(toDay),'low') 
+    openData = getStockListPriceTSL(stockList,int(begDay),int(toDay),'open') 
     
     now = datetime.datetime.now()     
     print now.strftime('%Y-%m-%d %H:%M:%S') +" Begin -------->"  
     #pool = Pool(processes = 3)  
-    toDay = now.strftime('%Y%m%d') 
+    
     #toDay="20170926"
     Path = 'd:\\python\\ConceptWIND\\'+toDay    
 
@@ -323,7 +333,9 @@ if __name__ == "__main__":
         tmpDataFrame['low'] = np.round(tmpDataFrame['low'],2) 
         tmpDataFrame['high'] = np.round(tmpDataFrame['high'],2) 
         tmpDataFrame['open'] = np.round(tmpDataFrame['open'],2) 
-        tmpDataFrame.fillna(method='pad')
+        
+        tmpDataFrame.replace(0,np.nan,inplace=True)
+        tmpDataFrame.fillna(method='pad',inplace=True)
         
         cloumns= cloumn.split('/')
         
@@ -378,11 +390,33 @@ if __name__ == "__main__":
             else :
                 PointX.plotPointX2(data1,Path+tmpPath,cloumns[0]) 
                 
+# =============================================================================
+#             if len(data1.index):
+#                 data1[u"日期"] = toDay
+#                 data1[u"股票代码"] = cloumn
+#                 data1.to_sql('OXdataDetails',engine,if_exists='append') 
+#             if len(lineDatas)>0:
+#                 lines = []
+#                 for idx, lineData in enumerate(lineDatas) :
+#                     line = pd.DataFrame(lineData,index=['X','Y']).T
+#                     line['ID'] = idx
+#                     lines.append(line)
+#                 lineData = pd.concat(lines)    
+#                 lineData[u"日期"] = toDay
+#                 lineData[u"股票代码"] = cloumn
+#                 lineData.to_sql('OXLineDatas',engine,if_exists='append') 
+# =============================================================================
+                
         except Exception ,e:
             print e.args
-            ErrorStocks.append(cloumn)
-    results = pd.DataFrame([UpStocks,DownStocks,noDireStocks,QianZaiStocks],index=[u'上涨',u'下跌','支撑阻力均有','无支撑和阻力']).T
+            ErrorStocks.append(cloumn) 
+    results = pd.DataFrame([UpStocks,DownStocks,noDireStocks,QianZaiStocks],\
+                           index=[u'上涨',u'下跌','支撑阻力均有','无支撑和阻力']).T
+    results[u"日期"] = toDay
     results.to_excel(Path+'\\results.xlsx') 
+    if len(results.index) :
+        results.to_sql('OXResults',engine,if_exists='append') #存入数据库，这句有时候运行一次报错，运行第二次就不报错了，不知道为什么  
+
     #stockLastPrice.to_excel(Path+'\\PointX_Simple_StockLastPrice.xlsx') 
     now = datetime.datetime.now() 
     print now.strftime('%Y-%m-%d %H:%M:%S') +" End-------->"
